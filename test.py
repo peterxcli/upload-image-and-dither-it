580125694dc37c053a2bb5c255a1af4435ae12ac
peterxcli <peterxcli@gmail.com>
2023-01-19 19:08:07 +0800

    auto create ./static/upload

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import json
 import uuid
 
 ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png'}
-
+if not os.path.exists('static/upload'): os.mkdir('static/upload')
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
